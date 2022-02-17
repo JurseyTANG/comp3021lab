b75ccc66c5f288e9e9d8c91988d0444a3cedaabc
JurseyTANG <92135036+JurseyTANG@users.noreply.github.com>
2022-02-17 21:49:11 +0800

    comp3021_lab1

---
+++ src/base/NoteBook.java
@@ -0,0 +1,36 @@
+package base;
+import java.util.ArrayList;
+public class NoteBook {
+	private ArrayList<Folder> folders;
+	NoteBook(){
+		folders= new ArrayList<Folder>();
+	}
+	public boolean createTextNote(String folderName,String title) {
+		TextNote note=new TextNote(title);
+		return insertNote(folderName,note);
+	}
+	public boolean createImageNote(String folderName,String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName,note);
+	}
+	public ArrayList<Folder> getFolder(){
+		return folders;
+	}
+	public boolean insertNote(String folderName,Note note) {
+		Folder f=null;
+		for(Folder f1:folders) {
+			if(f1.getName().equals(folderName)) {f=f1;break;}
+		}
+		if(f==null) {
+			f = new Folder(folderName);
+		}
+		for(Note n:f.getNotes()) {
+			if(n.equals(note)) {
+				System.out.println("Creating note "+note.getTitle()+" under folder "+folderName+" failed");
+				return false;
+			}
+		}
+		f.addNote(note);
+		return true; 
+	}
+}
---
+++ src/base/TextNote.java
@@ -0,0 +1,8 @@
+package base;
+
+public class TextNote extends Note{
+	String content;
+	TextNote(String s){
+		super(s);
+	}
+}
